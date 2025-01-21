cb2d4d8158167b50fec982816a91548dee25666c
MD Rifat hossain <106229451+withrifat@users.noreply.github.com>
2025-01-21 06:28:24 +0600

    Add html file in module 3

---
+++ Milestone 6 introduction js/module 3 js fundamentals/templeString.js
@@ -0,0 +1,14 @@
+let FirstName = 'Rifat';
+let MultiLine = `hi
+it's me 
+i am rifat
+`;
+let firstName = "MD. Rifat";
+let lastName = "Hossain";
+console.log(`My name is ${firstName} ${lastName}`);
+// interpulation with mathmatical oparation
+console.log(`The sum of 2+3 is ${2 + 3}`);
+// Interpulation
+let text = `hellow ${FirstName}, nice to meet you!`;
+console.log(text);
+console.log(MultiLine);
