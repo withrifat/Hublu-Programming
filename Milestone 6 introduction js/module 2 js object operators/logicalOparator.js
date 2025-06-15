97b723198f6e45141ee191758e0d23716d7c85be
withrifat <mdrifathassain97880@gmail.com>
2025-06-15 15:53:12 +0600

    update logical operator

--- Milestone 6 introduction js/module 2 js object operators/logicalOparator.js
+++ Milestone 6 introduction js/module 2 js object operators/logicalOparator.js
@@ -10,3 +10,14 @@ console.log(required && eligible) //whn both are true then op is true
 //  { logical OR }      ||
 console.log(required || eligible) //whn any one is true then op is true
 
+console.log(true && false); // when both are false then op is false
+console.log(! true); // oposite of false is true
+
+count = 0;
+let result = count || 10;
+console.log(result); 
+
+let result1 = true;
+let result2 = false;
+let result3 = result1 ?? result2;
+console.log(result3); 
