609cac6575e93824038ddec6271d82dbb5788215
withrifat <mdrifathassain97880@gmail.com>
2025-06-17 15:43:19 +0600

    add switch case  and ternary html module 4

---
+++ Milestone 6 introduction js/module 4/main.js
@@ -0,0 +1,15 @@
+let math = Math.random(4.7);
+let math2 =math.round();
+console.log(math2);
+
+
+// Using Math functions
+console.log("Square root of 16:", Math.sqrt(16)); // 4
+console.log("Round 4.7:", Math.round(4.7)); // 5
+console.log("Floor 4.7:", Math.floor(4.7)); // 4
+console.log("Ceil 4.1:", Math.ceil(4.1)); // 5
+console.log("Random number (0 to 1):", Math.random()); // random number
+
+// Max and Min
+console.log("Max:", Math.max(3, 5, 9)); // 9
+console.log("Min:", Math.min(3, 5, 9)); // 3
